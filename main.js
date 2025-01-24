69fb68a3d321d096c5f9bb0f11e5d41e20a07408
Uniqxxvii <rvtt27@gmail.com>
2025-01-23 21:42:52 -0800

    finalcommit

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-const test = arr => {
+const day = Math.floor(Math.random() * 30) +1;
+const month = ['January', 'February', 'March', 'April', 'June', 'July', 'August', 'September', 'November', 'December'];
+const year = Math.floor(Math.random() * 101) + 1900;
 
+function getRandomMonth(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+}
+const randomMonth = getRandomMonth(month);
+
+const getMessage = (obj1, obj2, obj3) => {
+    return 'Today is ' + obj1 + ' of ' + obj2 + ' ' + obj3;
 }
 
-const result = test();
-console.log('Hello World!');
+const result = getMessage(day, randomMonth, year);
+console.log(result);
